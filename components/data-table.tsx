b23f9d87e5211eb58ce39eac098278c1b43ebe7f
Abel Gideon <abelgideontk7@gmail.com>
2025-10-26 12:48:12 +0300

    fix: Update Datatable full row to be clickable

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -13,13 +13,15 @@ import { Status, Priority, taskSchemaTableType } from "@/lib/zodSchemas";
 import { formatRelativeTime } from "@/lib/utils";
 import { TASK_PRIORITY, TASK_STATUS } from "@/db/schema";
 import { Badge } from "@/components/ui/badge";
+import { useRouter } from "next/navigation";
 
 export function DataTable({ data }: { data: taskSchemaTableType[] }) {
+  const router = useRouter();
   return (
     <div className="overflow-x-auto rounded-lg border">
       <Table>
         <TableHeader>
-          <TableRow>
+          <TableRow className="bg-muted/50">
             <TableHead>Title</TableHead>
             <TableHead>Priority</TableHead>
             <TableHead>Status</TableHead>
@@ -32,12 +34,9 @@ export function DataTable({ data }: { data: taskSchemaTableType[] }) {
               <TableRow
                 key={task.id}
                 className="hover:bg-muted/50 cursor-pointer transition"
+                onClick={() => router.push(`/tasks/${task.id}`)}
               >
-                <TableCell>
-                  <Link href={`/tasks/${task.id}`} className="hover:underline">
-                    {task.title}
-                  </Link>
-                </TableCell>
+                <TableCell>{task.title}</TableCell>
                 <TableCell>
                   <Badge>
                     {TASK_PRIORITY[task.priority as Priority].label}
